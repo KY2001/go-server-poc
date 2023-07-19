12d66235e389568f1c0034b5826d41f514073288
KY2001 <yukiyoshikein@gmail.com>
2023-07-19 17:17:01 +0900

    add error handling when client == nil

--- infrastructure/firebase/firebase.go
+++ infrastructure/firebase/firebase.go
@@ -2,6 +2,7 @@ package firebase
 
 import (
 	"context"
+	"log"
 
 	firebase "firebase.google.com/go/v4"
 	"firebase.google.com/go/v4/auth"
@@ -16,14 +17,18 @@ func InitClient() {
 	var err error
 	app, err = firebase.NewApp(context.Background(), nil)
 	if err != nil {
-		panic("Failed to initialize firebase app.")
+		log.Fatalf("InitClient: Failed to initialize firebase app: %v\n", err)
 	}
 	client, err = app.Auth(context.Background())
 	if err != nil {
-		panic("Failed to initialize firebase client.")
+		log.Fatalf("InitClient: Failed to initialize firebase client: %v\n", err)
 	}
 }
 
 func GetClient() *auth.Client {
+	if client == nil {
+		InitClient()
+		log.Println("GetClient: Firebase client should be initialized when server starts.")
+	}
 	return client
 }
